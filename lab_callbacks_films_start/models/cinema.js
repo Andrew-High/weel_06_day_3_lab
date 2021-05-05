6a2ba3314cae4b9de465529ef2261046ed2ab682
crispymccoy145 <samspam19941@gmail.com>
2021-05-05 15:02:04 +0100

    2 tests left to go

--- lab_callbacks_films_start/models/cinema.js
+++ lab_callbacks_films_start/models/cinema.js
@@ -7,9 +7,23 @@ Cinema.prototype.listFilmTitles = function () {
   return films;
 };
 
+// do we need a const inside the higher order function iteration variable name
 Cinema.prototype.findFilm = function (filmName){
   const film = this.films.find(item => item.title === filmName);
   return film;
+};
+
+Cinema.prototype.filterByGenre = function (genre){
+  const films = this.films.filter(item => item.genre === genre);
+  return films;
+};
+
+Cinema.prototype.checkForYear = function (year){
+  const answer = this.films.find(item => item.year === year);
+  if (answer){
+    return true;
+  };
+  return false;
 };
 
 module.exports = Cinema;
--- lab_callbacks_films_start/specs/cinema_spec.js
+++ lab_callbacks_films_start/specs/cinema_spec.js
@@ -36,9 +36,20 @@ describe('Cinema', function () {
     const actual = cinema.findFilm('Dunkirk');
     assert.deepStrictEqual(actual, dunkirk)
   });
-  it('should be able to filter films by genre');
-  it('should be able to check whether there are some films from a particular year');
-  it('should be able to check whether there are no films from a particular year');
+
+  it('should be able to filter films by genre', function (){
+    const actual = cinema.filterByGenre('drama');
+    assert.deepStrictEqual(actual, [moonlight, trainspotting]);
+  });
+  
+  it('should be able to check whether there are some films from a particular year', function (){
+    const actual = cinema.checkForYear(2017);
+    assert.strictEqual(actual, true);
+  });
+  it('should be able to check whether there are no films from a particular year', function (){
+    const actual = cinema.checkForYear(2010);
+    assert.strictEqual(actual, false);
+  });
   it('should be able to check whether all films are over a particular length');
   it('should be able to calculate total running time of all films');
 
